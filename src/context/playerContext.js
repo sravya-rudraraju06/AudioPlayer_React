88d1b37d097bc61f55f40ba416c9787d41519d7f
sravya-rudraraju <106981368+sravya-rudraraju@users.noreply.github.com>
2022-06-06 23:37:55 +0530

    Update playerContext.js

--- src/context/playerContext.js
+++ src/context/playerContext.js
@@ -3,7 +3,3 @@ import { createContext } from 'react';
 const playerContext = createContext();
 
 export default playerContext;
-
-// Before having contols component
-// we need to click the playlist so that Which song is been playingg We must noww..
-//Ccreate context floder(3 components in them)
